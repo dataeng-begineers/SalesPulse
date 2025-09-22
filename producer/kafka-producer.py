95643109e8d264e96f2ef2b7136fa49d9d6fba8b
Rafo044 <rafo044@protonmail.com>
2025-09-22 20:25:30 +0400

    Add try and except method on kafka connect response

--- producer/kafka-producer.py
+++ producer/kafka-producer.py
@@ -4,17 +4,24 @@ import json
 import time
 
 # Kafka setup
-producer = KafkaProducer(
-    bootstrap_servers='kafka:9092',  # Use service name from docker-compose
-    value_serializer=lambda v: json.dumps(v).encode('utf-8')
-)
+for i in range(10):
+    try:
+        producer = KafkaProducer(
+            bootstrap_servers='kafka:9092',
+            value_serializer=lambda v: json.dumps(v).encode('utf-8')
+        )
+        print("Connected to Kafka!")
+        break
+    except:
+        print("Kafka not ready, retrying...")
+        time.sleep(5)
 
 while True:
     try:
         # Fetch latest data from FastAPI service
-        response = requests.get("http://fastapi-service:8000/latest")
+        response = requests.get("http://fastapi_service:8000/latest")
         data = response.json()
-        
+
         # Send each record to Kafka
         for record in data:
             producer.send('sales_topic', record)
@@ -25,5 +32,3 @@ while True:
     except Exception as e:
         print(f"Error fetching/sending data: {e}")
         time.sleep(5)
-
-
